50fca1f5c3fe7eb0508be4b5c0a3aea1444fa526
romV2K <konopelkoroman99@gmail.com>
2021-03-19 17:56:47 +0200

    follow unfollow added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,43 +5,67 @@ import userIcon from '../../assets/images/userIcon.svg'
 import { NavLink } from 'react-router-dom';
 import ReactPaginate from 'react-paginate';
 import Preloader from '../../assets/preloader/Preloader'
+import * as axios from 'axios'
 
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+  
 
   return (
     <main className={styles.usersMainContainer}>
       <ReactPaginate
-          previousLabel={'<'}
-          nextLabel={'>'}
-          previousClassName={styles.prevButton}
-          nextClassName={styles.nextButton}
-          breakLabel={'...'}
-          breakClassName={'break-me'}
-          pageCount={pagesCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={props.onPageChanged}
-          containerClassName={styles.pagination}
-          subContainerClassName={'pages pagination'}
-          activeClassName={styles.selected}
-        />
-      {props.isFetching ? <Preloader/> : null}
+        previousLabel={'<'}
+        nextLabel={'>'}
+        previousClassName={styles.prevButton}
+        nextClassName={styles.nextButton}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={pagesCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={props.onPageChanged}
+        containerClassName={styles.pagination}
+        subContainerClassName={'pages pagination'}
+        activeClassName={styles.selected}
+      />
+      {props.isFetching ? <Preloader /> : null}
       <div className={props.isFetching ? 'hideAndNone' : `${styles.users} show`}>
         {
           props.users.map(u =>
             <div className={styles.user} key={u.id}>
               <span className={styles.avatarAndFollowButton}>
-                <NavLink to={'/profile/'+u.id}>
+                <NavLink to={'/profile/' + u.id}>
                   <div className={styles.avatarContainer}>
                     <img className={styles.avatar} src={u.photos.small != null ? u.photos.small : userIcon} alt={u.name + '`s avatar'}></img>
                   </div>
                 </NavLink>
                 <div>
                   {u.followed
-                    ? <button onClick={() => props.unfollow(u.id)} className={styles.followButton}>unfollow</button>
-                    : <button onClick={() => props.follow(u.id)} className={styles.followButton}>follow</button>
+                    ? <button onClick={() => {
+                      axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                        headers: {
+                          'API-KEY': 'ceb868d5-e092-40d3-a57e-f46c94cb7815'
+                        },
+                        withCredentials: true
+                      })
+                        .then(response => {
+                          if (response.data.resultCode === 0)
+                            props.unfollow(u.id)
+                        })
+                    }} className={styles.followButton}>unfollow</button>
+                    : <button onClick={() => {
+                      axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                        headers: {
+                          'API-KEY': 'ceb868d5-e092-40d3-a57e-f46c94cb7815'
+                        },
+                        withCredentials: true
+                      })
+                        .then(response => {
+                          if (response.data.resultCode === 0)
+                            props.follow(u.id)
+                        })
+                    }} className={styles.followButton}>follow</button>
                   }
                 </div>
               </span>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -7,7 +7,9 @@ import {follow, unfollow, setUsers, setCurrentPage, setTotalUsersCount, setIsFet
 class UsersContainer extends React.Component {
   componentDidMount(){
     this.props.setIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,{
+      withCredentials: true
+    })
       .then(response => {
         this.props.setIsFetching(false)
         this.props.setUsers(response.data.items)
@@ -18,7 +20,9 @@ class UsersContainer extends React.Component {
     let selected = pageObj.selected + 1 
     this.props.setCurrentPage(selected)
     this.props.setIsFetching(true)
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${selected}&count=${this.props.pageSize}`)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${selected}&count=${this.props.pageSize}`,{
+      withCredentials: true
+    })
     .then(response => {
         this.props.setIsFetching(false)
         this.props.setUsers(response.data.items)
